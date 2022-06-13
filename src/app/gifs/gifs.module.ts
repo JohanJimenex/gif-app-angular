db132286beb712160dcaf8bc3bb7ab5b1425a078
JohanJimenex <johan.jimenez.23@gmail.com>
2022-06-13 00:16:59 -0400

    first commit

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifspageComponent } from './gifspage/gifspage.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifspageComponent,
+    BuscarComponent,
+    ResultadosComponent
+  ],
+  exports: [
+    GifspageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }

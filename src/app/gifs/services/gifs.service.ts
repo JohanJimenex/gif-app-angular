e97ffc6dee636761ed571e0f16ba45940b8a961c
johanjimenex <johan.jimenez.23@gmail.com>
2022-06-14 21:51:19 -0400

    g

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -2,13 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-
 @Injectable({
-  //Con esta propiedad no es necesario colocarlo en Providers en el modulo, Valido para Angular  
+  //Con esta propiedad no es necesario colocarlo en Providers en el modulo, Valido para Angular version x
   providedIn: 'root'
 })
 export class GifsService {
-
 
   private urlBase: string = "https://api.giphy.com/v1/gifs/search";
   private apiKey: string = "S5AT6UKQ8VX4NxpEPQYGOLE4m0EV1Oob";
@@ -23,14 +21,9 @@ export class GifsService {
 
   constructor(private http: HttpClient) { }
 
-
-
   buscarGif(query: string): void {
 
     query = query.trim().toLowerCase();
-
-
-
 
     this.http.get(`${this.urlBase}?api_key=${this.apiKey}&q=${query}&limit=20`)
       .subscribe((resp: any) => {
